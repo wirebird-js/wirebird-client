a84facb38b427b4d97011d4bb0bb13a0da7a15a2
no <no>
2019-07-28 12:06:05 +0300

    add snapshots, add test of POST request

--- __tests__/GlobalHttpLogger.spec.js
+++ __tests__/GlobalHttpLogger.spec.js
@@ -1,109 +1,72 @@
 const axios = require('axios');
 const sleep = require('sleep-promise');
 const GlobalHttpLogger = require('../lib/GlobalHttpLogger');
-const { removeUnstableData } = require('./utils');
+const { prepareSnapshot } = require('./utils');
 
-it('should capture get requests', async () => {
-    const onRequestEnd = jest.fn();
-    const logger = new GlobalHttpLogger({ onRequestEnd });
-    logger.start();
-    const res = await axios.get('http://127.0.0.1:13000/get?a=b');
-    expect(res.data).toEqual('Hello World!');
-    expect(res.status).toEqual(200);
+describe('GlobalHttpLogger', () => {
+    it('should capture get requests', async () => {
+        const onRequestEnd = jest.fn();
+        const logger = new GlobalHttpLogger({ onRequestEnd });
+        logger.start();
+        const res = await axios.get('http://127.0.0.1:13000/get?a=b');
+        expect(res.data).toEqual('Hello World!');
+        expect(res.status).toEqual(200);
 
-    await sleep(100);
-    expect(removeUnstableData(onRequestEnd.mock.calls[0][0])).toEqual({
-        error: null,
-        request: {
-            url: 'http://127.0.0.1:13000/get?a=b',
-            method: 'GET',
-            headers: {
-                accept: 'application/json, text/plain, */*',
-                host: '127.0.0.1:13000',
-                'user-agent': 'axios/0.19.0'
-            },
-            body: null
-        },
-        response: {
-            status: 200,
-            headers: {
-                connection: 'close',
-                'content-length': '12',
-                'content-type': 'text/html; charset=utf-8',
-                'x-powered-by': 'Express'
-            },
-            body: Buffer.from('Hello World!')
-        }
+        await sleep(100);
+        expect(
+            prepareSnapshot(onRequestEnd.mock.calls[0][0])
+        ).toMatchSnapshot();
     });
-});
 
-it('should capture get requests with error response', async () => {
-    const onRequestEnd = jest.fn();
-    const logger = new GlobalHttpLogger({ onRequestEnd });
-    logger.start();
-    const res = await axios.get('http://127.0.0.1:13000/get/404', {
-        validateStatus: () => true
+    it('should capture post requests', async () => {
+        const onRequestEnd = jest.fn();
+        const logger = new GlobalHttpLogger({ onRequestEnd });
+        logger.start();
+        const res = await axios.post('http://127.0.0.1:13000/post', {
+            foo: 'bar'
+        });
+        expect(res.data).toEqual({ hello: 'world' });
+        expect(res.status).toEqual(200);
+
+        await sleep(100);
+        expect(
+            prepareSnapshot(onRequestEnd.mock.calls[0][0])
+        ).toMatchSnapshot();
     });
-    expect(res.data).toEqual({ error: 'Not found' });
-    expect(res.status).toEqual(404);
 
-    await sleep(100);
-    expect(removeUnstableData(onRequestEnd.mock.calls[0][0])).toEqual({
-        error: null,
-        request: {
-            url: 'http://127.0.0.1:13000/get/404',
-            method: 'GET',
-            headers: {
-                accept: 'application/json, text/plain, */*',
-                host: '127.0.0.1:13000',
-                'user-agent': 'axios/0.19.0'
-            },
-            body: null
-        },
-        response: {
-            status: 404,
-            headers: {
-                connection: 'close',
-                'content-length': '21',
-                'content-type': 'application/json; charset=utf-8',
-                'x-powered-by': 'Express'
-            },
-            body: Buffer.from(JSON.stringify({ error: 'Not found' }))
-        }
-    });
-});
-
-it('should capture get requests with network error', async () => {
-    const onRequestEnd = jest.fn();
-    const logger = new GlobalHttpLogger({ onRequestEnd });
-    logger.start();
-
-    await expect(
-        axios.get('http://never.existing.host.asdfgh/', {
+    it('should capture get requests with error response', async () => {
+        const onRequestEnd = jest.fn();
+        const logger = new GlobalHttpLogger({ onRequestEnd });
+        logger.start();
+        const res = await axios.get('http://127.0.0.1:13000/get/404', {
             validateStatus: () => true
-        })
-    ).rejects.toThrow(
-        'getaddrinfo ENOTFOUND never.existing.host.asdfgh never.existing.host.asdfgh:80'
-    );
+        });
+        expect(res.data).toEqual({ error: 'Not found' });
+        expect(res.status).toEqual(404);
 
-    await sleep(100);
+        await sleep(100);
+        expect(
+            prepareSnapshot(onRequestEnd.mock.calls[0][0])
+        ).toMatchSnapshot();
+    });
 
-    expect(removeUnstableData(onRequestEnd.mock.calls[0][0])).toEqual({
-        error: {
-            code: 'ENOTFOUND',
-            message:
-                'getaddrinfo ENOTFOUND never.existing.host.asdfgh never.existing.host.asdfgh:80'
-        },
-        request: {
-            url: 'http://never.existing.host.asdfgh/',
-            method: 'GET',
-            headers: {
-                accept: 'application/json, text/plain, */*',
-                host: 'never.existing.host.asdfgh',
-                'user-agent': 'axios/0.19.0'
-            },
-            body: null
-        },
-        response: null
+    it('should capture get requests with network error', async () => {
+        const onRequestEnd = jest.fn();
+        const logger = new GlobalHttpLogger({ onRequestEnd });
+        logger.start();
+
+        await expect(
+            axios.get('http://never.existing.host.asdfgh/', {
+                validateStatus: () => true
+            })
+        ).rejects.toThrow(
+            'getaddrinfo ENOTFOUND never.existing.host.asdfgh never.existing.host.asdfgh:80'
+        );
+
+        await sleep(100);
+
+        expect(
+            prepareSnapshot(onRequestEnd.mock.calls[0][0])
+        ).toMatchSnapshot();
     });
 });
--- __tests__/utils.js
+++ __tests__/utils.js
@@ -1,6 +1,6 @@
 const _ = require('lodash');
 
-exports.removeUnstableData = input => {
+const removeUnstableData = input => {
     const output = _.cloneDeep(input);
     if (output.response) {
         if (output.response.headers) {
@@ -14,3 +14,20 @@ exports.removeUnstableData = input => {
 
     return output;
 };
+
+const withReadableBuffers = input => {
+    const output = _.cloneDeep(input);
+    if (output.request && output.request.body) {
+        output.request.body = output.request.body.toString('utf8');
+    }
+    if (output.response && output.response.body) {
+        output.response.body = output.response.body.toString('utf8');
+    }
+    return output;
+};
+
+const prepareSnapshot = input => withReadableBuffers(removeUnstableData(input));
+
+exports.removeUnstableData = removeUnstableData;
+exports.withReadableBuffers = withReadableBuffers;
+exports.prepareSnapshot = prepareSnapshot;
