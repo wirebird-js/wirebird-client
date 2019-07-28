e13c0ea5bcd090b33cb0a8ec6319fc6422674196
no <no>
2019-07-28 16:21:58 +0300

    ignore output

--- lib/GlobalHttpLogger.d.ts
+++
@@ -1,9 +0,0 @@
-import { LoggerOnResponseCallback } from './SharedTypes';
-export default class GlobalHttpLogger {
-    onRequestEnd: LoggerOnResponseCallback;
-    constructor({ onRequestEnd }: {
-        onRequestEnd: LoggerOnResponseCallback;
-    });
-    start(): void;
-    stop(): void;
-}
--- lib/__tests__/utils.d.ts
+++
@@ -1,4 +0,0 @@
-import { LoggerOnResponsePayload } from '../src/SharedTypes';
-export declare const removeUnstableData: (input: LoggerOnResponsePayload) => LoggerOnResponsePayload;
-export declare const withReadableBuffers: (input: LoggerOnResponsePayload) => LoggerOnResponsePayload;
-export declare const prepareSnapshot: (input: LoggerOnResponsePayload) => LoggerOnResponsePayload;
